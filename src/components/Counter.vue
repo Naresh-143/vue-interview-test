586fa22530592981c9c9959679b1dda0a00b2a13
Naresh-143 <nareshpulkam8@gmail.com>
2025-04-07 22:38:14 +0530

    Initial commit

---
+++ src/components/Counter.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="mb-4">
+      <h5>Count: {{ counter.count }}</h5>
+      <button class="btn btn-primary" @click="counter.increment">Increment</button>
+    </div>
+  </template>
+  
+  <script setup>
+  import { useCounterStore } from '../store/counter'
+  const counter = useCounterStore()
+  </script>
+  
